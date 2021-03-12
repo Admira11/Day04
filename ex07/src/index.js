28f6e433d7a747cb0f512a0464a54ebdee0e30b3
Admira11 <80249903+Admira11@users.noreply.github.com>
2021-03-12 11:06:00 +0100

    Add files via upload

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    var numOne;
+    var numTwo;
+    var remainder = numOne % numTwo;
+    //Only change code above this line
+    return remainder;
+
+}
+console.log(main(5, 2));
+console.log(main(15, 4));
+console.log(main(1002, 24));   
+module.exports = main;
---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+    //Only change code below this line
+    var firstNum = 6;
+    var secondNum =25;
+    var thirdNum=5.2;
+    firstNum = firstNum * 9;
+    secondNum = secondNum / 5;
+    thirdNum= thirdNum * 10;
+    //Only change code above this line
+    return {
+        firstNum,
+        secondNum,
+        thirdNum
+    };
+
+}
+console.log(main());
+   
+module.exports = main;
