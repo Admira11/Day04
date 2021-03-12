aecc486f7e50d34b25f6cde5ea33b6d45890a6bd
Admira11 <80249903+Admira11@users.noreply.github.com>
2021-03-12 11:10:54 +0100

    Add files via upload

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    //Only change code below this line
+    var myFirstName = "Iva";
+    var myLastName = "Kos";
+    var myHomeTown = "Zeenica";
+    //Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+
+}
+console.log(main());
+   
+module.exports = main;
